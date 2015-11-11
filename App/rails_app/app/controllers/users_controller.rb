d677f7f67be0884f1a0d83c188b5f957c3982916
Nilton Vasques <nilton.vasques@gmail.com>
2015-11-11 20:48:20 -0300

    Bloqueado o acesso a pagina de administração para usuários admin. #15

--- App/rails_app/app/controllers/users_controller.rb
+++ App/rails_app/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user, only: [:edit, :update]
-  before_filter :admin_user, only: :destroy
+  before_filter :admin_user, only: [:destroy, :index]
   before_filter :block_signed_create, only: [:new, :create]
 
   def new
